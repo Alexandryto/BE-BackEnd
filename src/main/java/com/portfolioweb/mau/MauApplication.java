0f3e5dfd7edba0a192051c52fbe597775a90ec63
MauroAlde <48041709+Alexandryto@users.noreply.github.com>
2023-01-12 18:44:20 -0300

    inicio BackEnd

---
+++ src/main/java/com/portfolioweb/mau/MauApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioweb.mau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolioweb/mau/MauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioweb.mau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
